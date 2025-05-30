19235092043ca5a22c5cef39fc81dd0eaea2eb16
ExcellentDev <sean939393@proton.me>
2025-05-30 13:31:13 -0700

    update

--- app.js
+++ app.js
@@ -58,8 +58,8 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use("/api", makeCallRouter);
 app.use("/api", callStatusRouter);
 
